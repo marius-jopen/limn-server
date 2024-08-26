2a6a6d1d57201e5f93475dcd68c477229f18401b
Marius Jopen <mail@mariusjopen.com>
2024-08-26 22:11:22 +0200

    setting up deofruim

--- routes/generateRoutes.js
+++ routes/generateRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import generateImage1111Local from '../components/generate-image-1111-local.js';
 import generateImage1111RunPodServerless from '../components/generate-image-1111-runpod-serverless.js';
-import generateImage1111RunpodPod from '../components/generate-image-1111-runpod-pod.js'; // Import the new function
+import generateImage1111RunpodPod from '../components/generate-image-1111-runpod-pod.js';
+import generateDeforum1111RunpodPod from '../components/generate-deforum-1111-runpod-pod.js'; // Import the new function
 
 const router = express.Router();
 
 // POST endpoint for generating images locally
 router.post('/generate-image-1111-local', async (req, res) => {
   try {
-    const imageRequest = req.body;  // Receive the whole object
+    const imageRequest = req.body;
     const { imageUrl, info } = await generateImage1111Local(imageRequest);
-
     res.json({ imageUrl, info });
   } catch (error) {
     console.error('Error in /generate-image-1111-local:', error);
@@ -21,9 +21,8 @@ router.post('/generate-image-1111-local', async (req, res) => {
 // POST endpoint for generating images on RunPod serverless
 router.post('/generate-image-1111-runpod-serverless', async (req, res) => {
   try {
-    const imageRequest = req.body;  // Receive the whole object
+    const imageRequest = req.body;
     const { imageUrl, info } = await generateImage1111RunPodServerless(imageRequest);
-
     res.json({ imageUrl, info });
   } catch (error) {
     console.error('Error in /generate-image-1111-runpod-serverless:', error);
@@ -34,12 +33,23 @@ router.post('/generate-image-1111-runpod-serverless', async (req, res) => {
 // POST endpoint for generating images on RunPod real pod
 router.post('/generate-image-1111-runpod-pod', async (req, res) => {
   try {
-    const imageRequest = req.body;  // Receive the whole object
-    const { imageUrl, info } = await generateImage1111RunpodPod(imageRequest); // Call the new function
-    
+    const imageRequest = req.body;
+    const { imageUrl, info } = await generateImage1111RunpodPod(imageRequest);
     res.json({ imageUrl, info });
   } catch (error) {
     console.error('Error in /generate-image-1111-runpod-pod:', error);
+    res.status(500).json({ message: 'Error generating or saving the image.' });
+  }
+});
+
+// POST endpoint for generating images using Deforum on RunPod real pod
+router.post('/generate-deforum-1111-runpod-pod', async (req, res) => {
+  try {
+    const imageRequest = req.body;
+    const { imageUrl, info } = await generateDeforum1111RunpodPod(imageRequest);
+    res.json({ imageUrl, info });
+  } catch (error) {
+    console.error('Error in /generate-deforum-1111-runpod-pod:', error);
     res.status(500).json({ message: 'Error generating or saving the image.' });
   }
 });
